f2d372f4e67d36ee7d3f449eebb8b394adc2a017
z.zamani <z_zamani_91@yahoo.com>
2021-09-05 17:09:32 +0430

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import QRCode from "react-qr-code";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+ReactDOM.render(<QRCode value="4000410" />, document.getElementById("root"));
+/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
